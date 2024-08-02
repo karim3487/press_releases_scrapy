17b84016c8e7fffbf478725ce20b35db2ae54969
Karim Kabirov <karim3487@gmail.com>
2024-08-02 14:08:36 +0600

    Prettify code.

--- press_releases/db.py
+++ press_releases/db.py
@@ -5,7 +5,9 @@ import config_reader as cfg
 from press_releases.models import Base
 
 sqlite_uri = "sqlite:///press_rel.sqlite3"
-postgresql_uri = f"postgresql://{cfg.USERNAME}:{cfg.PASSWORD}@{cfg.HOST}:{cfg.PORT}/{cfg.DB_NAME}"
+postgresql_uri = (
+    f"postgresql://{cfg.USERNAME}:{cfg.PASSWORD}@{cfg.HOST}:{cfg.PORT}/{cfg.DB_NAME}"
+)
 
 engine = create_engine(postgresql_uri, echo=False)
 Base.metadata.create_all(engine)
